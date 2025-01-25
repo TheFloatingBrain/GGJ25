f0b2ccd90cccc91f62b9f5eee76e8b46aafa3c97
TheFloatingBrain <1550298+TheFloatingBrain@users.noreply.github.com>
2025-01-24 22:18:49 -0500

    Initial Commit

---
+++ include/bubbles/common.hpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+#include <string_view>
+#include <sstream>
+#include <vector>
+#include <memory>
+#include <variant>
+
---
+++ scratchpad/main.cpp
@@ -0,0 +1,8 @@
+#include <bubbles/common.hpp>
+#include <r3d.h>
+int main(int argc, char** args) {
+	std::cout << "Hello World\n";
+
+	return 0;
+}
+
